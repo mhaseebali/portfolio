b45ab477d69fbcff38d32d9569c2accf90974fe3
mhaseebali <haseebmlk12@gmail.com>
2022-11-25 02:26:39 +0500

    make it better3

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     # path('', views.req),
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logOutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
     path('', views.req1),
     path('designs/', views.req2),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+# from django.contrib.auth.forms import UserCreationForm
+from . forms import CustomUserCreationForm
 
 # Create your views here.
 
@@ -7,6 +11,67 @@ from django.shortcuts import render
 #     return render(request, 'home.html', {
 #       "sometext": "challenge_text"
 #     })
+
+
+
+
+def loginUser(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('../')
+
+
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'user does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('../')
+        else:
+            messages.error(request, 'username or password is incorrect')
+
+    return render(request, 'myapp/login_register.html')
+    
+
+
+def logOutUser(request):
+    logout(request)
+    messages.error(request, 'user was logged out')
+    return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('../')
+        
+        else:
+            messages.success(request, 'An error was occurred during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'myapp/login_register.html', context)
+
 
 def req1(request):
     return render(request, 'myapp/base.html')
